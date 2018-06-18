03310ded79814aa478aab79137deee7abea0c0a2
DOMAIN\dallan <dallan@youngevity.com>
2018-06-18 08:07:49 -0700

    updates

---
+++ Utilities/Helpers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace ExpensesMVC2018.Utilities
+{
+    public class Helpers
+    {
+        public static byte[] CreateArrayFromImage(Image img)
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                img.Save(memoryStream, ImageFormat.Jpeg);
+                return memoryStream.ToArray();
+            }
+        }
+    }
+}
--- Utilities/ImageResizer.cs
+++ Utilities/ImageResizer.cs
@@ -18,12 +18,12 @@ namespace ExpensesMVC2018.Utilities
         /// <param name="img">image to save</param>  
         public static void SaveJpeg(string path, Image img)
         {
-            var qualityParam = new EncoderParameter(Encoder.Quality, 100L);
-            var jpegCodec = GetEncoderInfo("image/jpeg");
+            var QualityParam = new EncoderParameter(Encoder.Quality, 100L);
+            var JpegCodec = GetEncoderInfo("image/jpeg");
 
             var encoderParams = new EncoderParameters(1);
-            encoderParams.Param[0] = qualityParam;
-            img.Save(path, jpegCodec, encoderParams);
+            encoderParams.Param[0] = QualityParam;
+            img.Save(path, JpegCodec, encoderParams);
         }
 
         /// <summary>  
@@ -44,18 +44,18 @@ namespace ExpensesMVC2018.Utilities
         /// <returns></returns>
         public static Bitmap ResizeImage(Image image, double percentage)
         {
-            double fractionalPercentage = (percentage / 100.0);
+            double PercentageToUse = (percentage / 100.0);
 
-            int width = (int)(image.Width * fractionalPercentage);
+            int ImageWidth = (int)(image.Width * PercentageToUse);
 
-            int height = (int)(image.Height * fractionalPercentage);
+            int ImageHeight = (int)(image.Height * PercentageToUse);
 
-            var destRect = new Rectangle(0, 0, width, height);
-            var destImage = new Bitmap(width, height);
+            var NewRectangle = new Rectangle(0, 0, ImageWidth, ImageHeight);
+            var NewImage = new Bitmap(ImageWidth, ImageHeight);
 
-            destImage.SetResolution(72, 72);
+            NewImage.SetResolution(72, 72);
 
-            using (var graphics = Graphics.FromImage(destImage))
+            using (var graphics = Graphics.FromImage(NewImage))
             {
                 graphics.CompositingMode = CompositingMode.SourceCopy;
                 graphics.CompositingQuality = CompositingQuality.HighQuality;
@@ -66,57 +66,61 @@ namespace ExpensesMVC2018.Utilities
                 using (var wrapMode = new ImageAttributes())
                 {
                     wrapMode.SetWrapMode(WrapMode.TileFlipXY);
-                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                    graphics.DrawImage(image, NewRectangle, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
                 }
             }
 
-            return destImage;
+            return NewImage;
         }
 
         public static void DoImageResize(string pFilePath, string pFileName, string pOutputFileName)
         {
 
-            System.Drawing.Image imgBef;
-            imgBef = System.Drawing.Image.FromFile(pFilePath + pFileName);
+            System.Drawing.Image image;
+            image = System.Drawing.Image.FromFile(pFilePath + pFileName);
 
             //pWidth = imgBef.Width * 50 / 100;
             //pHeight = imgBef.Height * 50 / 100;
 
-            System.Drawing.Image _imgR;
-            _imgR = ResizeImage(imgBef, 15); //Imager.Resize(imgBef, pWidth, pHeight, true);
+            System.Drawing.Image ImageResized;
+            ImageResized = ResizeImage(image, 15); //Imager.Resize(imgBef, pWidth, pHeight, true);
 
             //System.Drawing.Image _img2;
             //_img2 = Imager.PutOnCanvas(_imgR, pWidth, pHeight, System.Drawing.Color.White);
 
             //Save JPEG  
-            SaveJpeg(pFilePath + pOutputFileName, _imgR);
+            SaveJpeg(pFilePath + pOutputFileName, ImageResized);
 
         }
 
+        /// <summary>
+        /// Create and resize image from stream.
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <returns></returns>
         public static byte[] ImageResize(Stream stream)
         {
 
-            System.Drawing.Image imgBef;
+            // Create new image from incoming stream
+            System.Drawing.Image ImageFromStream;
             //imgBef = System.Drawing.Image.FromFile(pFilePath);
-            imgBef = System.Drawing.Image.FromStream(stream);
+            ImageFromStream = System.Drawing.Image.FromStream(stream);
 
-            //pWidth = imgBef.Width * 50 / 100;
-            //pHeight = imgBef.Height * 50 / 100;
-
-            System.Drawing.Image _imgR;
-            _imgR = ResizeImage(imgBef, 15); //Imager.Resize(imgBef, pWidth, pHeight, true);
-
-            //System.Drawing.Image _img2;
-            //_img2 = Imager.PutOnCanvas(_imgR, pWidth, pHeight, System.Drawing.Color.White);
+            // Create resized image
+            System.Drawing.Image ResizedImage;
+            ResizedImage = ResizeImage(ImageFromStream, 15); //Imager.Resize(imgBef, pWidth, pHeight, true);
 
             //Save JPEG  
             //SaveJpeg(pFilePath + pOutputFileName, _imgR);
 
-            using (MemoryStream memoryStream = new MemoryStream())
-            {
-                _imgR.Save(memoryStream, ImageFormat.Jpeg);
-                return memoryStream.ToArray();
-            }
+            // Return image as a byte array
+            // Make this a utility method? return ConvertedImage();
+            return Helpers.CreateArrayFromImage(ResizedImage);
+            //using (MemoryStream memoryStream = new MemoryStream())
+            //{
+            //    _imgR.Save(memoryStream, ImageFormat.Jpeg);
+            //    return memoryStream.ToArray();
+            //}
 
             //Byte[] result = (Byte[])new ImageConverter().ConvertTo(_imgR, typeof(Byte[]));
 
